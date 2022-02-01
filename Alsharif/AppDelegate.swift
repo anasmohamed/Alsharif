102c5eadb4d6b94d52f97ede35be489e3d4112db
Anas Mohamed <anaskamal659@gmail.com>
2022-02-01 15:02:25 -0800

    tab bar design

--- Alsharif/AppDelegate.swift
+++ Alsharif/AppDelegate.swift
@@ -16,6 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         UITabBar.appearance().unselectedItemTintColor = .white
+
         GMSServices.provideAPIKey(AppConfig.googleMapKey)
         GMSPlacesClient.provideAPIKey(AppConfig.googlePlacesKey)
         return true
